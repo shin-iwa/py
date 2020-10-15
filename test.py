ee8faa4f27e70737fedd0d59583b87cdef9ae4fd
shin-iwa <luckystar0443@gmail.com>
2020-10-15 22:58:53 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+test = ['pasta', 'curry', 'sushi']
+print(test)
+print(test[0])
+
+fruits = ['apple', 'banana', 'orange', 'grape']
+food = 'sushi'
+
+for fruit in fruits:
+    print('好きなものは' + food + 'と'+ fruit + 'です')
