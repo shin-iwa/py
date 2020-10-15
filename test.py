0ffdfadf594f79aed7825de024f4f2a3a4dee4cb
shin-iwa <luckystar0443@gmail.com>
2020-10-15 23:14:36 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,5 +5,7 @@ print(test[0])
 fruits = ['apple', 'banana', 'orange', 'grape']
 food = 'sushi'
 
+i = 0
 for fruit in fruits:
-    print('好きなものは' + food + 'と'+ fruit + 'です')
+    print(str(i) + '.' +'好きなものは' + food + 'と'+ fruit + 'です')
+    i += 1
