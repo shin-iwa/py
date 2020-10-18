cccaa6230e6d29ab6872d7e2404c3282241cc7e7
shin-iwa <luckystar0443@gmail.com>
2020-10-18 18:08:22 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,14 @@ test = ['pasta', 'curry', 'sushi']
 print(test)
 print(test[0])
 
-fruits = ['apple', 'banana', 'orange', 'grape']
-food = 'sushi'
+# fruits = ['apple', 'banana', 'orange', 'grape']
+fruits = [{'sushi': 'apple'}, {'curry': 'banana'}, {'sandwitch': 'orange'}]
+# food = 'sushi'
 # foods = ['sushi', 'teriyaki']
 
-i = 0
-for fruit in fruits:
-    print(str(i) + '.' +'好きなものは' + food + 'と'+ fruit + 'です')
-    i += 1
+# i = 0
+# for fruit in fruits:
+#     print(str(i) + '.' +'好きなものは' + fruit + 'です')
+#     i += 1
+
+print(fruits[0])
