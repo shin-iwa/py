49f514f3832420127d7b4779424a889acfb031ca
shin-iwa <luckystar0443@gmail.com>
2020-10-24 18:10:28 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,10 +1,9 @@
 class Main {
   public static void main(String[] args) {
-    String word;
-    word = "hello";
-    System.out.println(word);
+    
+    for(int i = 1; i <= 10; i++) {
+      System.out.println(i + "回目のループです");
+    }
 
-    boolean bool = 4 + 3 == 8;
-    System.out.println(bool);
   }
 }
