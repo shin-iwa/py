0ebfd7a8628fa77a90b418e8920f39fd15447ea8
shin-iwa <luckystar0443@gmail.com>
2020-10-24 17:47:46 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,10 @@
-String word;
-word = "hello";
-System.out.println(word);
+class Main {
+  public static void main(String[] args) {
+    String word;
+    word = "hello";
+    System.out.println(word);
 
-boolean bool = 4 + 3 == 8;
-System.out.println(bool);
+    boolean bool = 4 + 3 == 8;
+    System.out.println(bool);
+  }
+}
