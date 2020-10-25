52e2cb1b4cc42425b39329fba8d064d4e3b30459
shin-iwa <luckystar0443@gmail.com>
2020-10-25 20:24:43 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
 
-    String[]names = {"ichieo", "haruka", "jiro"};
+    String[]names = {"ichieo", "haruka", "jiro", "saburo"};
     for(String name: names){
       System.out.println(name);
     }
