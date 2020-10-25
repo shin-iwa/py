d1802db8452ba9fd253830c4348a7e80ba059ac8
shin-iwa <luckystar0443@gmail.com>
2020-10-25 09:25:04 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,10 +1,6 @@
 class Main {
   public static void main(String[] args) {
 
-    for(int i = 1; i <= 10; i++) {
-      System.out.println(i + "回目のループです");
-    }
-
     String[]names = {"ichieo", "haruka", "jiro"};
     for(String name: names){
       System.out.println(name);
