e3784ec2dbf41ab8e7505bde5ee9902b10a865c6
shin-iwa <luckystar0443@gmail.com>
2020-10-29 22:05:06 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
+x = 22
 
-num01 = ["sato", "suzuki"]
-print(type(num01))
-print(num01[1])
-num01[1] = "tanaka"
-print(num01)
-
-x = 10
-y = 2
-x += 10
-print(x == 10 and y == 2)
+if(x >= 20):
+  print("adult")
