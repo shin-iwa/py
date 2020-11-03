e9d8b76e77ab92927e5b30374d77dd282a708acb
shin-iwa <luckystar0443@gmail.com>
2020-11-03 22:43:40 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,3 +7,10 @@ def unit(a, b, c):
 print(avr(9, 10, 2))
 
 print(unit("hana", "mizu", "ki"))
+
+class Student:
+  def avg(self, math, english):
+    print((math + english)/2)
+
+a001 = Student()
+a001.avg(80, 60)
