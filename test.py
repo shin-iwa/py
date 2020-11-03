c3a5f9134ba1848e9d89c64463943023b73654dc
shin-iwa <luckystar0443@gmail.com>
2020-11-04 00:39:19 +0900

    Update test.py

--- test.py
+++ test.py
@@ -9,17 +9,15 @@ print(avr(9, 10, 2))
 print(unit("hana", "mizu", "ki"))
 
 class Student:
-  def __init__(self):
-    self.name = ""
+  def __init__(self,name):
+    self.name = name
 
   def avg(self, math, english):
     print((math + english)/2)
 
-a001 = Student()
-a001.name = "sato"
+a001 = Student("sato")
 print(a001.name)
 
-a002 = Student()
-a002.name = "tanaka"
+a002 = Student("tanaka")
 print(a002.name)
 
