eff749b5d14278655408382c5bf3fd1184fbfce4
shin-iwa <luckystar0443@gmail.com>
2020-11-21 15:27:46 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,4 @@
-def add(num01, num02):
-  return(num01 + num02)
+def ave(num01, num02, num03):
+  return (num01 + num02 + num03)/3
 
-print(add(6, 2))
-
-add_result = add(6, 2)
-print(add_result)
-
-
-
-
-
-
-
-
-
+print(ave(4, 6, 7))
