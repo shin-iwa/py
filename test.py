37731e2ed519109029599e8cac61af5def4e8442
shin-iwa <luckystar0443@gmail.com>
2020-11-21 11:42:05 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
-x = 10
-y = 2
+x = 8
+y = 3
 
-print(x < y)
+print(x >= 5 and x >= 3)
 
-print(x <= y)
-print(x >= y)
-
-print(x == y)
-print(x != y)
+print(x == 3 or y == 3)
+print(x == 1 or y == 1)
