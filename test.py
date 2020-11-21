e3d6d4e6c642979b01d7164747d1506883c18b72
shin-iwa <luckystar0443@gmail.com>
2020-11-21 18:38:42 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,13 @@ class Student:
   def avg(self,math,english):
       print((math + english)/2)
 
-a001 = Student()
-a001.avg(30, 70)
+  def __init__(self):
+    self.name = ""
 
+a001 = Student()
 a001.name = "sato"
 print(a001.name)
+
+a002 = Student()
+a002.name = "tanaka"
+print(a002.name)
